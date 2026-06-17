ba62c6fdac9dcd6e8bbedc91f072d58678976461
dansmash54 <dansmash@yandex.ru>
2026-06-17 21:14:06 +0300

    HAR TEST 2

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   webServer: {
     command: 'npm run start -- --no-client-overlay',
     url: 'http://localhost:4000',
-    reuseExistingServer: true,
+    reuseExistingServer: false,
     timeout: 120000,
   },
 });
